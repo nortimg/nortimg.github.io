0a53d3dc40b6d9abf9215cc7dcbb04b3305f8fb5
Alexey Gmitron <agmitron@sinergo.ru>
2020-07-23 10:22:35 +0700

    showing components in document

--- fingerprint-js/fp2.js
+++ fingerprint-js/fp2.js
@@ -1,6 +1,7 @@
 if (window.requestIdleCallback) {
     requestIdleCallback(function () {
         Fingerprint2.get(function (components) {
+          document.write(components)
           console.log(components) // an array of components: {key: ..., value: ...}
           const hash = window.murmurHash3.x64.hash128(components.join(''))
           document.write(hash)
