1e716694e5b8fc82baea3d8f98cfed7330556cd4
Alexey Gmitron <agmitron@sinergo.ru>
2020-10-30 19:27:38 +0700

    add the first version of the pet service

---
+++ pet-service/precache-manifest.b2d01fad22848a122f1078924fd8232f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ea146f6e8587a56d4716eba9424f66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79e7d50b6f5e06d2b192",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "79e7d50b6f5e06d2b192",
+    "url": "/static/js/2.f06203c3.chunk.js"
+  },
+  {
+    "revision": "6553848ede33e774934d8afdf700acf9",
+    "url": "/static/js/2.f06203c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "630b583d9757c05c03ae",
+    "url": "/static/js/main.4ff86988.chunk.js"
+  },
+  {
+    "revision": "02abd13eaf1d7c33000d",
+    "url": "/static/js/runtime-main.6a771f16.js"
+  }
+]);
